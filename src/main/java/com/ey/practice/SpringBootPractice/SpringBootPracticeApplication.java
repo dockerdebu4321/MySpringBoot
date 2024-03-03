b43347b5adeddf5274786c67f414ed55346fc9f3
dockerdebu4321 <86752499+dockerdebu4321@users.noreply.github.com>
2024-03-03 22:14:32 +0530

    INC2000 : Initial changes commit

---
+++ src/main/java/com/ey/practice/SpringBootPractice/SpringBootPracticeApplication.java
@@ -0,0 +1,15 @@
+package com.ey.practice.SpringBootPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringBootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ey/practice/SpringBootPractice/service/EmployeeService.java
@@ -0,0 +1,134 @@
+package com.ey.practice.SpringBootPractice.service;
+
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ey.practice.SpringBootPractice.bean.EmployeeBean;
+import com.ey.practice.SpringBootPractice.model.EmployeeEntity;
+import com.ey.practice.SpringBootPractice.repository.EmployeeRepository;
+@Service
+public class EmployeeService {
+	
+	
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+	public List<EmployeeBean> getAllEmployee() {
+		
+		List<EmployeeBean> beanList=new ArrayList<>();
+		
+		List<EmployeeEntity> entityList = employeeRepository.findAll();
+		
+		for (EmployeeEntity currentEmpEntity : entityList) {
+
+			EmployeeBean empBean = new EmployeeBean();
+			empBean.setEmployeeid(currentEmpEntity.getEid());
+			empBean.setFirstName(currentEmpEntity.getfName());
+			empBean.setLastName(currentEmpEntity.getlName());
+			empBean.setAddress(currentEmpEntity.getAdd());
+			empBean.setCity(currentEmpEntity.getCity());
+			if (empBean.getLastName().equalsIgnoreCase("lname2")) {
+				empBean.setCaste("Brahmin");
+			} else {
+				empBean.setCaste("NonBrahmin");
+			}
+			if (empBean.getEmployeeid() % 200 == 0) {
+				empBean.setEmpIdEven(true);
+			} else {
+				empBean.setEmpIdEven(false);
+			}
+			beanList.add(empBean);
+		}
+		return beanList;
+	}
+
+	public EmployeeBean getEmployeeById(int empId) {
+
+		Optional<EmployeeEntity> empEntityNullCheck = employeeRepository.findById(empId);
+		EmployeeBean  empBean = new EmployeeBean();
+		try {
+			EmployeeEntity empEntity = empEntityNullCheck.get();
+			empBean.setEmployeeid(empEntity.getEid());
+			empBean.setFirstName(empEntity.getfName());
+			empBean.setLastName(empEntity.getlName());
+			empBean.setAddress(empEntity.getAdd());
+			empBean.setCity(empEntity.getCity());
+			if (empBean.getLastName().equalsIgnoreCase("lname2")) {
+				empBean.setCaste("Brahmin");
+			} else {
+				empBean.setCaste("NonBrahmin");
+			}
+			if (empBean.getEmployeeid() % 200 == 0) {
+				empBean.setEmpIdEven(true);
+			} else {
+				empBean.setEmpIdEven(false);
+			}
+		} catch (Exception e) {
+			System.out.println("Enter valid employeeid");		    
+		}
+
+		return empBean;
+	}
+
+	public EmployeeBean updateEmployee(String firstName, String lastName,int employeeID) {
+		employeeRepository.updateEmployee(firstName, lastName,employeeID);
+		
+		Optional<EmployeeEntity> empEntityNullCheck = employeeRepository.findById(employeeID);
+		EmployeeBean  empBean = new EmployeeBean();
+		try {
+			EmployeeEntity empEntity = empEntityNullCheck.get();
+			empBean.setEmployeeid(empEntity.getEid());
+			empBean.setFirstName(empEntity.getfName());
+			empBean.setLastName(empEntity.getlName());
+			empBean.setAddress(empEntity.getAdd());
+			empBean.setCity(empEntity.getCity());
+			if (empBean.getLastName().equalsIgnoreCase("lname2")) {
+				empBean.setCaste("Brahmin");
+			} else {
+				empBean.setCaste("NonBrahmin");
+			}
+			if (empBean.getEmployeeid() % 200 == 0) {
+				empBean.setEmpIdEven(true);
+			} else {
+				empBean.setEmpIdEven(false);
+			}
+		} catch (Exception e) {
+			System.out.println("Enter valid employeeid");		    
+		}
+
+		return empBean;
+	}
+
+	public void deleteEmployee(int employeeID) {
+		employeeRepository.deleteById(employeeID);
+		
+	}
+
+	public int createEmployee(EmployeeBean eBean) {
+		
+		EmployeeEntity empEntity =new EmployeeEntity();
+		empEntity.setfName(eBean.getFirstName());
+		empEntity.setlName(eBean.getLastName());
+		empEntity.setAdd(eBean.getAddress());
+		empEntity.setCity(eBean.getCity());
+		
+		EmployeeEntity empSave=employeeRepository.save(empEntity);
+		return empSave.getEid();
+	}
+	
+	
+	
+
+	
+
+	
+	
+	
+	
+
+}
