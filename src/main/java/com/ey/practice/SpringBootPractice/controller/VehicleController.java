35d470fa0121faf33694704bf5ec02826d33d708
dockerdebu4321 <86752499+dockerdebu4321@users.noreply.github.com>
2024-03-04 08:50:40 +0530

    INC2002-added welcome message in vehicle controller

--- src/main/java/com/ey/practice/SpringBootPractice/controller/VehicleController.java
+++ src/main/java/com/ey/practice/SpringBootPractice/controller/VehicleController.java
@@ -1,5 +1,23 @@
 package com.ey.practice.SpringBootPractice.controller;
 
-public class VehicleController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ey.practice.SpringBootPractice.bean.EmployeeBean;
+import com.ey.practice.SpringBootPractice.service.EmployeeService;
+
+@RestController
+@RequestMapping(path="/Vehicle")
+public class VehicleController{ 
+	
+	@GetMapping(path="/Welcome")
+	public String WelcomeMessage() {
+		
+		return "Welcome to Vehicle controller";
+	}
+	
 }
